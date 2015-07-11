tomasf/microprint

commit 595b3650850a30d7e13e380bca7186c043cfef5d

Temperature adjustments and fixes

microprint/TFPFilament.m
@@ -1,0 +1,92 @@
+//
+//  TFPFilament.m
+//  microprint
+//
+//  Created by Tomas Franzén on Sat 2015-07-11.
+//  Copyright (c) 2015 Tomas Franzén. All rights reserved.
+//
+
+#import "TFPFilament.h"
+
+
+@interface TFPFilament ()
+@property (readwrite) TFPFilamentType type;
+@property (readwrite, copy) NSString *name;
+
+@property (readwrite) double temperature;
+@end
+
+
+
+@implementation TFPFilament
+
+
++ (NSDictionary*)filamentSpecification {
+	return @{
+			 @(TFPFilamentTypePLA):
+				 @{
+					 @"name": @"PLA",
+					 @"temperature": @215,
+					 },
+			 @(TFPFilamentTypeABS):
+				 @{
+					 @"name": @"ABS",
+					 @"temperature": @275,
+					 },
+			 @(TFPFilamentTypeHIPS):
+				 @{
+					 @"name": @"HIPS",
+					 @"temperature": @265,
+					 },
+			 @(TFPFilamentTypeOther):
+				 @{
+					 @"name": @"Other",
+					 @"temperature": @215,
+					 },
+			 };
+}
+
+
++ (instancetype)defaultFilament {
+	return [self filamentForType:TFPFilamentTypePLA];
+}
+
+
++ (instancetype)filamentForType:(TFPFilamentType)type {
+	return [[self alloc] initWithType:type data:[self filamentSpecification][@(type)]];
+}
+
+
++ (TFPFilamentType)typeForString:(NSString*)string {
+	NSDictionary *names = @{
+							@"pla" : @(TFPFilamentTypePLA),
+							@"abs" : @(TFPFilamentTypeABS),
+							@"hips" : @(TFPFilamentTypeHIPS),
+							@"other" : @(TFPFilamentTypeOther),
+							};
+	NSNumber *type = names[string.lowercaseString];
+	return type ? type.integerValue : TFPFilamentTypeUnknown;
+}
+
+
+- (instancetype)initWithType:(TFPFilamentType)type data:(NSDictionary*)spec {
+	if(!spec) {
+		return nil;
+	}
+	
+	if(!(self = [super init])) return nil;
+	
+	self.type = type;
+	self.name = spec[@"name"];
+	self.temperature = [spec[@"temperature"] doubleValue];
+	
+	return self;
+}
+
+
+- (NSUInteger)fanSpeed {
+	return (self.type == TFPFilamentTypePLA) ? 255 : 50;
+}
+
+
+@end

microprint.xcodeproj/project.pbxproj
@@ -28,6 +28,7 @@
 		C99E87251B3AB4CD0099E79E /* TFPDryRunPrinter.m in Sources */ = {isa = PBXBuildFile; fileRef = C99E87241B3AB4CD0099E79E /* TFPDryRunPrinter.m */; };
 		C99E87281B3ACFD10099E79E /* TFPBacklashPreprocessor.m in Sources */ = {isa = PBXBuildFile; fileRef = C99E87271B3ACFD10099E79E /* TFPBacklashPreprocessor.m */; };
 		C9C729C81B4E96CB00277F1F /* TFPPreprocessing.m in Sources */ = {isa = PBXBuildFile; fileRef = C9C729C71B4E96CB00277F1F /* TFPPreprocessing.m */; };
+		C9C729E11B516DA900277F1F /* TFPFilament.m in Sources */ = {isa = PBXBuildFile; fileRef = C9C729E01B516DA900277F1F /* TFPFilament.m */; };
 		C9E59B6C1B316AF400343D58 /* main.m in Sources */ = {isa = PBXBuildFile; fileRef = C9E59B6B1B316AF400343D58 /* main.m */; };
 		C9E59B741B316BE800343D58 /* TFPGCode.m in Sources */ = {isa = PBXBuildFile; fileRef = C9E59B731B316BE800343D58 /* TFPGCode.m */; };
 		C9E59B771B316F0D00343D58 /* Extras.m in Sources */ = {isa = PBXBuildFile; fileRef = C9E59B761B316F0D00343D58 /* Extras.m */; };
@@ -99,6 +100,8 @@
 		C99E87271B3ACFD10099E79E /* TFPBacklashPreprocessor.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = TFPBacklashPreprocessor.m; sourceTree = "<group>"; };
 		C9C729C61B4E96CB00277F1F /* TFPPreprocessing.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TFPPreprocessing.h; sourceTree = "<group>"; };
 		C9C729C71B4E96CB00277F1F /* TFPPreprocessing.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = TFPPreprocessing.m; sourceTree = "<group>"; };
+		C9C729DF1B516DA900277F1F /* TFPFilament.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TFPFilament.h; sourceTree = "<group>"; };
+		C9C729E01B516DA900277F1F /* TFPFilament.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = TFPFilament.m; sourceTree = "<group>"; };
 		C9E59B681B316AF400343D58 /* microprint */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.executable"; includeInIndex = 0; path = microprint; sourceTree = BUILT_PRODUCTS_DIR; };
 		C9E59B6B1B316AF400343D58 /* main.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = main.m; sourceTree = "<group>"; };
 		C9E59B721B316BE800343D58 /* TFPGCode.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TFPGCode.h; sourceTree = "<group>"; };
@@ -170,6 +173,10 @@
 				C99E87241B3AB4CD0099E79E /* TFPDryRunPrinter.m */,
 				C9FD1C571B395FD000F98FF1 /* TFPPrinterManager.h */,
 				C9FD1C581B395FD000F98FF1 /* TFPPrinterManager.m */,
+				C94F60141B3815290074EC41 /* TFPPrintParameters.h */,
+				C94F60151B3815290074EC41 /* TFPPrintParameters.m */,
+				C9C729DF1B516DA900277F1F /* TFPFilament.h */,
+				C9C729E01B516DA900277F1F /* TFPFilament.m */,
 			);
 			name = Printer;
 			sourceTree = "<group>";
@@ -190,8 +197,6 @@
 			children = (
 				C90072AC1B3BF6660097594F /* TFPPrintJob.h */,
 				C90072AD1B3BF6660097594F /* TFPPrintJob.m */,
-				C94F60141B3815290074EC41 /* TFPPrintParameters.h */,
-				C94F60151B3815290074EC41 /* TFPPrintParameters.m */,
 				C92B59DA1B4017DC00F38FA7 /* TFPRepeatingCommandSender.h */,
 				C92B59DB1B4017DC00F38FA7 /* TFPRepeatingCommandSender.m */,
 				C92B59E01B40252900F38FA7 /* TFPExtrusionOperation.h */,
@@ -370,6 +375,7 @@
 				C9E59B771B316F0D00343D58 /* Extras.m in Sources */,
 				C9FD1C601B39618300F98FF1 /* TFPPrinter.m in Sources */,
 				C99E87221B3AA56D0099E79E /* GBSettings.m in Sources */,
+				C9C729E11B516DA900277F1F /* TFPFilament.m in Sources */,
 				C9FD1C4B1B384ED400F98FF1 /* TFPBedCompensationPreprocessor.m in Sources */,
 				C9FD1C591B395FD000F98FF1 /* TFPPrinterManager.m in Sources */,
 				C92B59E81B4090D200F38FA7 /* TFPBedLevelCalibrationOperation.m in Sources */,

microprint/TFPBasicPreparationPreprocessor.m
@@ -14,12 +14,9 @@
 
 
 - (TFPGCodeProgram*)processUsingParameters:(TFPPrintParameters*)parameters {
-	BOOL isPLA = parameters.filamentType == TFPFilamentTypePLA;
-
 	NSMutableArray *output = [NSMutableArray new];
 	
-	NSUInteger fanSpeed = isPLA ? 255 : 50;
-	TFPGCode *fanLine = [[TFPGCode codeWithString:@"M106"] codeBySettingField:'S' toValue:fanSpeed];
+	TFPGCode *fanLine = [[TFPGCode codeWithString:@"M106"] codeBySettingField:'S' toValue:parameters.filament.fanSpeed];
 	[output addObject:fanLine];
 	
 	[output addObject:[TFPGCode codeWithString:@"M17"]]; // Enable motors
@@ -38,17 +35,11 @@
 	[output addObject:[TFPGCode codeWithString:@"G92 E0"]]; // Reset E to 0
 	[output addObject:[TFPGCode codeWithString:@"G90"]]; // Absolute mode
 	[output addObject:[TFPGCode codeWithString:@"G0 F2400"]]; // Feed rate 2400
-	[output addObject:[TFPGCode codeWithString:@"; can extrude"]]; // Hmm?
 
 	// Add program lines, filtering out those who try to control extruder temperature or fan speed
 	[output addObjectsFromArray:[self.program.lines tf_selectWithBlock:^BOOL(TFPGCode *line) {
-		if([line hasField:'M']) {
-			NSUInteger M = [line valueForField:'M'];
-			if(M == 104 || M == 106 || M == 107 || M == 109) {
-				return NO;
-			}
-		}
-		return YES;
+		NSInteger M = [line valueForField:'M' fallback:-1];
+		return (M != 104 && M != 106 && M != 107 && M != 109);
 	}]];
 	
 	[output addObject:[TFPGCode codeWithString:@"G91"]]; // Relative mode
@@ -59,12 +50,12 @@
 	
 	if (parameters.maxZ > 60) {
 		if (parameters.maxZ < 110) {
-			[output addObject:[TFPGCode codeWithString:@"G0 Z3 F2900"]]; // Move up to the back right
+			[output addObject:[TFPGCode codeWithString:@"G0 Z3 F2900"]]; // Move up
 		}
 		[output addObject:[TFPGCode codeWithString:@"G90"]]; // Absolute mode
 		[output addObject:[TFPGCode codeWithString:@"G0 X90 Y84"]]; // Move to the back right a safe distance
 	} else {
-		[output addObject:[TFPGCode codeWithString:@"G0 Z3 F2900"]]; // Move up to the back right
+		[output addObject:[TFPGCode codeWithString:@"G0 Z3 F2900"]]; // Move up
 		[output addObject:[TFPGCode codeWithString:@"G90"]]; // Absolute mode
 		[output addObject:[TFPGCode codeWithString:@"G0 X95 Y95"]]; // Move to the back right a safe distance
 	}

microprint/TFPBedLevelCalibrationOperation.m
@@ -39,7 +39,7 @@
 	static TFPGCodeProgram *program;
 	if(!program) {
 		NSString *string = (@"M106 \n"
-							@"M109 S210 \n"
+							@"M109 S215 \n"
 							@"G90 \n"
 							@"G0 X1 Y9.5 Z0.15 F900 \n"
 							@"G0 Z0.4 E6 \n"

microprint/TFPCLIController.m
@@ -166,7 +166,7 @@
 	TFLog(@"");
 	
 	TFLog(@"Commands:");
-	TFLog(@"  print <gcode-path> [--temperature 210] [--filament PLA] [--backlash] [--backlashSpeed 1200] [--wavebonding]");
+	TFLog(@"  print <gcode-path> [--temperature 215] [--filament PLA] [--backlash] [--backlashSpeed 1200] [--wavebonding]");
 	TFLog(@"    Prints a G-code file.");
 	
 	TFLog(@"  preprocess <gcode-path> [--output path]");
@@ -181,10 +181,10 @@
 	TFLog(@"  off");
 	TFLog(@"    Turn off fan, heater and motors.");
 	
-	TFLog(@"  extrude [--temperature 210]");
+	TFLog(@"  extrude [--temperature 215]");
 	TFLog(@"    Extrude filament. Useful for loading new filament.");
 	
-	TFLog(@"  retract [--temperature 210]");
+	TFLog(@"  retract [--temperature 215]");
 	TFLog(@"    \"Reverse extrude\" that feeds filament backwards. Useful for unloading filament.");
 	
 	TFLog(@"  raise [--height 70]");
@@ -194,7 +194,7 @@
 	TFLog(@"");
 	TFLog(@"Options:");
 	TFLog(@"  --dryrun: Don't connect to an actual printer; instead simulate a mock printer that echos sent G-codes.");
-	TFLog(@"  --temperature <number>: Heater temperature in degrees Celsius. Default is 210 for extrusion/retraction and varies depending on filament type for printing.");
+	TFLog(@"  --temperature <number>: Heater temperature in degrees Celsius. Default is 215 for extrusion/retraction and varies depending on filament type for printing.");
 	TFLog(@"  --filament <string>: Filament type. Valid options are PLA, ABS, HIPS and Other. Affects behavior in some preprocessors. Also sets default temperature.");
 	TFLog(@"  --wavebonding: Use wave bonding. On by default. Turn off with --wavebonding=0");
 	TFLog(@"  --backlash: Use backlash compensation. On by default. Turn off with --backlash=0");
@@ -212,14 +212,14 @@
 	params.useBacklashCompensation = [settings boolForKey:@"backlash"];
 	params.backlashCompensationSpeed = [settings floatForKey:@"backlashSpeed"];
 	
-	params.filamentType = [self parseFilamentType:[settings objectForKey:@"filament"]];
-	if(params.filamentType == TFPFilamentTypeUnknown) {
+	params.filament = [TFPFilament filamentForType:[TFPFilament typeForString:[settings objectForKey:@"filament"]]];
+	if(!params.filament) {
 		TFLog(@"Unknown filament type specified.");
 		exit(EXIT_FAILURE);
 	}
 	
 	double temperature = [settings floatForKey:@"temperature"];
-	params.idealTemperature = (temperature > 0) ? temperature : [self defaultTemperatureForFilament:params.filamentType];
+	params.idealTemperature = temperature;
 
 	return params;
 }
@@ -232,7 +232,7 @@
 		
 		double temperature = [settings floatForKey:@"temperature"];
 		if(temperature < 1) {
-			temperature = 210;
+			temperature = 215;
 		}
 		
 		self.extrusionOperation = [[TFPExtrusionOperation alloc] initWithPrinter:self.printer retraction:[command isEqual:@"retract"]];
@@ -398,28 +398,6 @@
 		}
 		exit(EXIT_SUCCESS);
 	}];
-}
-
-
-- (TFPFilamentType)parseFilamentType:(NSString *)string {
-	NSDictionary *names = @{
-							@"pla" : @(TFPFilamentTypePLA),
-							@"abs" : @(TFPFilamentTypeABS),
-							@"hips" : @(TFPFilamentTypeHIPS),
-							@"other" : @(TFPFilamentTypeOther),
-							};
-	NSNumber *type = names[string.lowercaseString];
-	return type ? type.integerValue : TFPFilamentTypeUnknown;
-}
-
-
-- (double)defaultTemperatureForFilament:(TFPFilamentType)type {
-	return [@{
-			  @(TFPFilamentTypePLA): @210,
-			  @(TFPFilamentTypeABS): @265,
-			  @(TFPFilamentTypeHIPS): @245,
-			  @(TFPFilamentTypeOther): @210,
-			  }[@(type)] doubleValue];
 }
 
 

microprint/TFPExtrusionOperation.m
@@ -27,7 +27,7 @@
 	if(!(self = [super init])) return nil;
 	
 	self.printer = printer;
-	self.temperature = 210;
+	self.temperature = 215;
 	self.retract = retract;
 	
 	return self;

microprint/TFPFilament.h
@@ -1,0 +1,34 @@
+//
+//  TFPFilament.h
+//  microprint
+//
+//  Created by Tomas Franzén on Sat 2015-07-11.
+//  Copyright (c) 2015 Tomas Franzén. All rights reserved.
+//
+
+@import Foundation;
+
+
+typedef NS_ENUM(NSUInteger, TFPFilamentType) {
+	TFPFilamentTypeUnknown,
+	TFPFilamentTypePLA,
+	TFPFilamentTypeABS,
+	TFPFilamentTypeHIPS,
+	TFPFilamentTypeOther,
+};
+
+
+
+@interface TFPFilament : NSObject
++ (instancetype)defaultFilament;
++ (instancetype)filamentForType:(TFPFilamentType)type;
+
++ (TFPFilamentType)typeForString:(NSString*)string;
+
+@property (readonly) TFPFilamentType type;
+@property (readonly, copy) NSString *name;
+
+@property (readonly) double defaultTemperature;
+
+@property (readonly) NSUInteger fanSpeed;
+@end

microprint/TFPPrintParameters.h
@@ -5,16 +5,8 @@
 //  Created by Tomas Franzén on Mon 2015-06-22.
 //
 
-#import <Foundation/Foundation.h>
-
-
-typedef NS_ENUM(NSUInteger, TFPFilamentType) {
-	TFPFilamentTypeUnknown,
-	TFPFilamentTypePLA,
-	TFPFilamentTypeABS,
-	TFPFilamentTypeHIPS,
-	TFPFilamentTypeOther,
-};
+@import Foundation;
+#import "TFPFilament.h"
 
 
 typedef struct {
@@ -32,7 +24,6 @@ typedef struct {
 } TFPBacklashValues;
 
 
-
 extern NSString *TFPBedLevelOffsetsDescription(TFPBedLevelOffsets offsets);
 extern NSString *TFPBacklashValuesDescription(TFPBacklashValues values);
 
@@ -41,8 +32,8 @@ extern NSString *TFPBacklashValuesDescription(TFPBacklashValues values);
 @property (readwrite) NSUInteger bufferSize;
 @property (readwrite) BOOL verbose;
 
-@property (readwrite) TFPFilamentType filamentType;
-@property (readwrite) double idealTemperature;
+@property (readwrite) TFPFilament *filament;
+@property (readwrite, nonatomic) double idealTemperature;
 @property (readwrite) double maxZ;
 
 @property (readwrite) TFPBedLevelOffsets bedLevelOffsets;

commit 35c6971b595d23f0242a9892c71dce8f7f763954

Fixed print temperature bug introduced in 595b365

microprint/TFPFilament.m
@@ -13,7 +13,7 @@
 @property (readwrite) TFPFilamentType type;
 @property (readwrite, copy) NSString *name;
 
-@property (readwrite) double temperature;
+@property (readwrite) double defaultTemperature;
 @end
 
 
@@ -78,7 +78,7 @@
 	
 	self.type = type;
 	self.name = spec[@"name"];
-	self.temperature = [spec[@"temperature"] doubleValue];
+	self.defaultTemperature = [spec[@"temperature"] doubleValue];
 	
 	return self;
 }

microprint/TFPRaiseHeadOperation.m
@@ -25,10 +25,8 @@ static const double raiseStep = 1;
 	__weak __typeof__(self) weakSelf = self;
 	__weak TFPPrinter *printer = self.printer;
 	double targetHeight = self.targetHeight;
-	
-	printer.verboseMode = YES;
-	
-	[printer fetchPositionWithCompletionHandler:^(BOOL success, TFP3DVector *position, NSNumber *E) {		
+		
+	[printer fetchPositionWithCompletionHandler:^(BOOL success, TFP3DVector *position, NSNumber *E) {
 		[printer setRelativeMode:NO completionHandler:^(BOOL success) {
 			__block double Z = position.z.doubleValue;
 			